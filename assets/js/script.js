3130252d26cfe07002148a7d3b883df102f0e7a8
rachaelbabister <>
2023-10-07 13:10:36 +0000

    Add structure of functions to script file

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+function displayHowToPlay() {
+
+}
+
+function displayLeaderboard() {
+
+}
+
+function checkName() {
+
+}
+
+function runPop() {
+
+}
+
+function runDivas() {
+
+}
+
+function runMovies() {
+
+}
+
+function displayScore() {
+
+}
+
+function runNext() {
+
+}
+
+function displayHome() {
+
+}
