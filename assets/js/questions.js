6978f044cef87b885b82854ad8a58589894d1103
rachaelbabister <>
2023-10-08 17:12:53 +0000

    Delete script functions, amend index.html file with new code for a simpler game

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,9 +1,29 @@
-// Pop Music Quiz Questions
+// Movie Quiz Questions
+const askQuiz = [
+    {
+        question: "Look for the bare necessities, the simple bare necessities........",
+        answer: [
+          {option: "forget about your money and your life", correct: false},
+          {option: "don't forget about your fork and spoon and knife", correct: false},
+          {option: "forget about your worries and your strife" correct: true}
+        ]
+    },
 
-const askPop = {
-    "question":
-    "answers":
-    "correct":
+    {
+        question: "His palms are sweaty, knees weak, ........",
+        answer: [
+          { option: "he's getting petty", correct: false },
+          { option: "arms are heavy", correct: true },
+          { option: "arms are waving", correct: false }
+        ]
+    },
 
-}
-
+    {
+        question: "Cause your fine and your mine ........",
+        answer: [
+          { option: "and you look so divine", correct: true },
+          { option: "just like a fine wine", correct: false },
+          { option: "will you be my valentine"; correct: false },
+        ]
+    }
+];
