81b6ce15eb48fa7d794691b51d1922d606a5e380
rachaelbabister <>
2023-10-08 17:20:41 +0000

    Add script structure

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+/* Variables to call on html */
+const quizQuestions = document.getElementById('quiz-questions');
+const quizOptions = document.getElementById('quiz-options');
+const nextQuestion = document.getElementById('next-btn');
+
+/* Start the quiz */
+function startQuiz() {
+
+}
+
+/* Display quiz questions */
+function showQuestion() {
+
+}
+
+/* Choose question options */
+function quizOptions() {
+
+}
+
+/* Show results of quiz */
+function showResults() {
+
+}
+
+/* Event listeners */
