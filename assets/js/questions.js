7e83889c6fcbf63d9ed076decc21332a7f8ac666
rachaelbabister <>
2023-10-09 10:00:26 +0000

    Add questions for the quiz to questions script file

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -25,5 +25,101 @@ const askQuiz = [
           { option: "just like a fine wine", correct: false },
           { option: "will you be my valentine", correct: false }
         ]
-    }
+    },
+        {
+            question: `"My boom-boom heart, it's a  ........ and I'm alive"`,
+        answer: [
+            { option: "rockin' and a rollin'", correct: false },
+            { option: "beat and its a thumping", correct: true },
+            { option: "will you be my valentine", correct: false }
+        ]
+    },
+        {
+            question: `"Clock strikes upon the hour, ........"`,
+        answer: [
+            { option: "and the sun begins to fade", correct: true },
+            { option: "and the moon begins to shine", correct: false },
+            { option: "and the wolves come out to play", correct: false }
+        ]
+    },
+        {
+            question: `"Swing it, shake it, ........ who do you think you are"`,
+        answer: [
+            { option: "twist it, bop it", correct: false },
+            { option: "are you going to make it", correct: false },
+            { option: "move it, make it", correct: true }
+        ]
+    },
+        {
+        question: `"I've got the key, ........"`,
+        answer: [
+            { option: "to open another door", correct: false },
+            { option: "to unlock your heart", correct: false },
+            { option: "I've got the secret", correct: true }
+        ]
+    },
+        {
+            question: `"Don't need money, ........, don't need no credit card to ride this train"`,
+        answer: [
+            { option: "so don't complain", correct: false },
+            { option: "don't take fame", correct: true },
+            { option: "we just want champagne", correct: false }
+        ]
+    },
+        {
+            question: `"Girls in white dresses with ........, snowflakes that stay on my nose and eyelashes"`,
+        answer: [
+            { option: "silky blue sashes", correct: false },
+            { option: "floating blue sashes", correct: false },
+            { option: "blue satin sashes", correct: true }
+        ]
+    },
+        {
+            question: `"........ but a second-hand emotion"`,
+        answer: [
+            { option: "What are we living for", correct: false },
+            { option: "What's love", correct: true },
+            { option: "This is nothing", correct: false }
+        ]
+    },
+        {
+            question: `"I never meant to cause you any ........, I never meant to cause you any ........"`,
+        answer: [
+            { option: "sorrow / pain", correct: true },
+            { option: "trouble / hurt", correct: false },
+            { option: "worries / rage", correct: false }
+        ]
+    },
+        {
+        question: `"I've been through the desert ........"`,
+        answer: [
+            { option: "ignoring the mirage", correct: false },
+            { option: "searching for life", correct: false },
+            { option: "on a horse with no name", correct: true }
+        ]
+    },
+        {
+            question: `"Stop, wait a minute, ........, put some liquor in it"`,
+        answer: [
+            { option: "go grab my cup", correct: false },
+            { option: "fill my cup", correct: true },
+            { option: "here's my glass", correct: false }
+        ]
+    },
+        {
+            question: `"I can take myself dancing, and I can ........"`,
+        answer: [
+            { option: "say things you don't understand", correct: false },
+            { option: "talk to myself for hours", correct: false },
+            { option: "hold my own hand", correct: true }
+        ]
+    },
+{
+    question: `"Tell me something boy, ........"`,
+        answer: [
+            { option: "is there something else you're searching for", correct: false },
+            { option: "aren't you tired trying to fill that void", correct: true },
+            { option: "what you doing with that girl", correct: false }
+        ];
+}
 ];
